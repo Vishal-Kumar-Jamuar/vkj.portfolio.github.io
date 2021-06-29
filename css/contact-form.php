5164cfae4b502df0856a9047f8a3cf9f520a28a4
Vishal Kumar Jamuar <vishujamuar05@gmail.com>
2021-06-29 18:59:05 +0530

    Update contact-form.php

--- css/contact-form.php
+++ css/contact-form.php
@@ -4,7 +4,7 @@
     $subject=$_POST['subject'];
     $message=$_POST['message'];
 
-    $email_from="vishal@portfolio.com";
+    $email_from="vishal-kumar-jamuar.github.io/vkj.portfolio.github.io";
     $email_body="User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                     "User Message:$message.\n";
