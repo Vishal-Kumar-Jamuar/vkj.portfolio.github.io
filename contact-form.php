7b99d3b3e7a377f68ce923e8cb35e3017e75d5b9
Vishal Kumar Jamuar <vishujamuar05@gmail.com>
2021-06-29 19:36:24 +0530

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,19 +1,17 @@
 <?php
-    $name=$POST['name'];
-    $visitor_email=$_POST['email'];
-    $subject=$_POST['subject'];
-    $message=$_POST['message'];
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
 
-    $email_from="vishal-kumar-jamuar.github.io/vkj.portfolio.github.io";
-    $email_body="User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                    "User Message:$message.\n";
+    
+$email_body="User Name:". $name . "\r\n User Email:". $visitor_email . "\r\n User Message:". $message .;
 
-    $to="vishujamuar05@gmail.com";
-    $headers="From:$email_from \r\n";
-    $headers="Reply-To:$visitor_email \r\n";
+$to="vishujamuar05@gmail.com";
+$headers="From:$email_from \r\n";
+$headers="Reply-To:$visitor_email \r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location:index.html");
+mail($to,$email_subject,$email_body,$headers);
+header("Location:index.html");
 
-    ?>
+?>
